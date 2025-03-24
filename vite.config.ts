718c37aa47e4383c320b8e235de4966b3bd8ad22
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-24 23:30:39 +0530

    refactor: setup aliases for all the pages, components, ui components and constants for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      '@pages': '/src/pages/index',
+      '@timer-ui': '/src/components/UI/index',
+      '@components': '/src/components',
+      '@constants': '/src/constants',
+    },
+  },
 });
