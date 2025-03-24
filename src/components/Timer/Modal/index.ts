b169bbff5894c714cd68e4414c0313ca67e98256
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-24 23:44:14 +0530

    refactor: create an index file in timer and timer modal components to re-export pages for easy imports

---
+++ src/components/Timer/Modal/index.ts
@@ -0,0 +1,4 @@
+import AddTimerModal from './AddTimerModal';
+import EditTimerModal from './EditTimerModal';
+
+export { AddTimerModal, EditTimerModal };
---
+++ src/components/Timer/index.ts
@@ -0,0 +1,7 @@
+import TimerList from './TimerList';
+import TimerItem from './TimerItem';
+import TimerProgress from './TimerProgress';
+import TimerControls from './TimerControls';
+import TimerBackground from './TimerBackground';
+
+export { TimerList, TimerItem, TimerProgress, TimerControls, TimerBackground };
