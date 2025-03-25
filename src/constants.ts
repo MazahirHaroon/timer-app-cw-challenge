26bf685e9054cf887ca49b7aa6d497ccfb2f4890
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-25 08:04:40 +0530

    refactor: move FormErrorType to type/ui, delete constants file and update imports accordingly

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export interface FormErrorType {
-  hasError?: boolean;
-  errorMessage?: string;
-}
--- src/types/ui.ts
+++ src/types/ui.ts
@@ -3,3 +3,7 @@ import React from 'react';
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
 }
+export interface FormErrorType {
+  hasError?: boolean;
+  errorMessage?: string;
+}
