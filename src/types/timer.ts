5c1c9a0ab749b27746e6421d20b04e00cb442035
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-25 06:54:08 +0530

    fix: remove duplicate timer interface

--- src/types/timer.ts
+++ src/types/timer.ts
@@ -8,14 +8,4 @@ interface Timer {
   createdAt: number;
 }
 
-interface Timer2 {
-  id: string;
-  title: string;
-  description: string;
-  duration: number; // in seconds
-  remainingTime: number;
-  isRunning: boolean;
-  createdAt: number;
-}
-
-export type { Timer, Timer2 };
+export type { Timer };
