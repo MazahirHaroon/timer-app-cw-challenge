156e24575a00a1cc01a19a97e1cc0380d0e91c84
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-25 17:11:16 +0530

    refactor: extract useTimerStore logic to hooks directory

---
+++ src/hooks/useTimerStore.tsx
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Timer } from '@types/timer';
+
+import {
+  addTimer,
+  deleteTimer,
+  toggleTimer,
+  updateTimer,
+  restartTimer,
+  editTimer,
+} from '@custom-hooks';
+
+export const useTimerStore = () => {
+  const dispatch = useDispatch();
+  const timers = useSelector((state: { timers: Timer[] }) => state.timers);
+
+  return {
+    timers,
+    addTimer: (timer: Omit<Timer, 'id' | 'createdAt'>) => dispatch(addTimer(timer)),
+    deleteTimer: (id: string) => dispatch(deleteTimer(id)),
+    toggleTimer: (id: string) => dispatch(toggleTimer(id)),
+    updateTimer: (id: string) => dispatch(updateTimer(id)),
+    restartTimer: (id: string) => dispatch(restartTimer(id)),
+    editTimer: (id: string, updates: Partial<Timer>) => dispatch(editTimer({ id, updates })),
+  };
+};
