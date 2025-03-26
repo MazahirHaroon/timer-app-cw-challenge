e89fc594c9e113965ecb8058fdab834045eb7ae2
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-26 13:15:43 +0530

    refactor: removed useEffect import used for debugging purposes

--- src/components/timer/Modal/TimerModal.tsx
+++ src/components/timer/Modal/TimerModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { useTimerStore } from '@custom-hooks';
 import { validateTimerForm } from '@utils/validation';
